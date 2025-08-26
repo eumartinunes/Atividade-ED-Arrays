729308a132142b50a60a1f455fbc2b4b74aa5011
eumartinunes <martin2006nunes09@gmail.com>
2025-08-26 00:02:05 -0300

    Questao02.java

--- Atividade-ED/Questao02.java
+++ Atividade-ED/Questao02.java
@@ -4,13 +4,12 @@ public class Questao02 {
     public static void main(String[] args) {
         double[] notas = {7.0, 8.5, 10.0, 6.5};
         double soma = 0;
-
+        
         for (int i = 0; i < 4; i++) {
             soma += notas[i];
         }
-
         double media = soma / 4;
-
         System.out.println("A média das notas é: " + media);
     }
 }
+
